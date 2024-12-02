a0a2378d90282dd3eccf3684def74289c24a3dd7
Melbin Vergara <92840083+Melb02@users.noreply.github.com>
2024-12-02 18:01:42 -0500

    Update guardar.js

--- guardar.js
+++ guardar.js
@@ -1,11 +1,12 @@
-db.collection("users").add({
-    first: "Ada",
-    last: "Lovelace",
-    born: 1815
-})
-.then((docRef) => {
-    console.log("Document written with ID: ", docRef.id);
-})
-.catch((error) => {
-    console.error("Error adding document: ", error);
-});
+function guardar(){
+    db.collection("Cliente").add({
+        nombre: "Ada",
+        telefono: "Lovelace"
+    })
+    .then((docRef) => {
+        console.log("Document written with ID: ", docRef.id);
+    })
+    .catch((error) => {
+        console.error("Error adding document: ", error);
+    });
+}
